1c2b20117066a18a7d105a7fe702b48684da7dbb
guomingyue <mingyuefly@qq.com>
2020-01-10 19:46:08 +0800

    light and shadow

---
+++ 变换/lightAndShadow/lightAndShadow/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  lightAndShadow
+//
+//  Created by Gguomingyue on 2020/1/10.
+//  Copyright © 2020 Gmingyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
--- 变换/lightAndShadow/lightAndShadow/ViewController.m
+++ 变换/lightAndShadow/lightAndShadow/ViewController.m
@@ -1,12 +1,16 @@
 //
 //  ViewController.m
-//  solid
+//  lightAndShadow
 //
 //  Created by Gguomingyue on 2020/1/10.
 //  Copyright © 2020 Gmingyue. All rights reserved.
 //
 
 #import "ViewController.h"
+#import <GLKit/GLKit.h>
+
+#define LIGHT_DIRECTION 0, 1, -0.5
+#define AMBIENT_LIGHT 0.5
 
 @interface ViewController ()
 
@@ -71,6 +75,68 @@
     [self.cubeContainerView addSubview:face];
     face.center = CGPointMake(self.cubeContainerView.bounds.size.width / 2, self.cubeContainerView.bounds.size.height / 2);
     face.layer.transform = transform;
+    [self applyLightingToFace:face];
+}
+
+-(void)applyLightingToFace:(UIView *)face
+{
+    CALayer *layer = [CALayer layer];
+    layer.frame = face.layer.bounds;
+    [face.layer addSublayer:layer];
+    
+    CATransform3D transform = face.layer.transform;
+    // 32位机器可以直接这样做
+    //GLKMatrix4 matrix4 = *(GLKMatrix4 *)&transform;
+    // 64位机器需要一个将CGFloat转为float的过程
+    struct CATransform3DByMe
+    {
+      float m11, m12, m13, m14;
+      float m21, m22, m23, m24;
+      float m31, m32, m33, m34;
+      float m41, m42, m43, m44;
+    };
+    struct CATransform3DByMe tempTransform;
+    for (int i = 0; i < 16; i++) {
+        tempTransform.m11 = transform.m11;
+        tempTransform.m12 = transform.m12;
+        tempTransform.m13 = transform.m13;
+        tempTransform.m14 = transform.m14;
+        
+        tempTransform.m21 = transform.m21;
+        tempTransform.m22 = transform.m22;
+        tempTransform.m23 = transform.m23;
+        tempTransform.m24 = transform.m24;
+        
+        tempTransform.m31 = transform.m31;
+        tempTransform.m32 = transform.m32;
+        tempTransform.m33 = transform.m33;
+        tempTransform.m34 = transform.m34;
+        
+        tempTransform.m41 = transform.m41;
+        tempTransform.m42 = transform.m42;
+        tempTransform.m43 = transform.m43;
+        tempTransform.m44 = transform.m44;
+    }
+    GLKMatrix4 matrix4 = *(GLKMatrix4 *)&tempTransform;
+    GLKMatrix3 matrix3 = GLKMatrix4GetMatrix3(matrix4);
+    //get face normal
+    GLKVector3 normal = GLKVector3Make(0, 0, 1);
+    normal = GLKMatrix3MultiplyVector3(matrix3, normal);
+    normal = GLKVector3Normalize(normal);
+    //get dot product with light direction
+    GLKVector3 light = GLKVector3Normalize(GLKVector3Make(LIGHT_DIRECTION));
+    float dotProduct = GLKVector3DotProduct(light, normal);
+    //set lighting layer opacity
+    CGFloat shadow = 1 + dotProduct - AMBIENT_LIGHT;
+    UIColor *color = [UIColor colorWithWhite:0 alpha:shadow];
+    layer.backgroundColor = color.CGColor;
+    
+//    UIColor *color = [UIColor greenColor];
+//    UIColor *customColor = [color colorWithAlphaComponent:shadow];
+//    layer.backgroundColor = customColor.CGColor;
+    
+    UILabel *label = (UILabel *)[face viewWithTag:1000];
+    [face.layer insertSublayer:label.layer above:layer];
 }
 
 -(void)createFaces
@@ -83,12 +149,14 @@
         //faceView.layer.borderWidth = 1.0f;
         [array addObject:faceView];
         UILabel *numLabel = [[UILabel alloc] init];
-        numLabel.backgroundColor = [UIColor clearColor];
+        //numLabel.backgroundColor = [UIColor whiteColor];
         numLabel.text = [NSString stringWithFormat:@"%d", i + 1];
         numLabel.textAlignment = NSTextAlignmentCenter;
         numLabel.textColor = [UIColor redColor];
         numLabel.font = [UIFont systemFontOfSize:40];
-        numLabel.frame = faceView.bounds;
+        numLabel.frame = CGRectMake(0, 0, 40, 40);
+        numLabel.center = faceView.center;
+        numLabel.tag = 1000;
         [faceView addSubview:numLabel];
     }
     self.faceArray = array;
